4826bcdd3ef9769f239a1eb15047eea411af28e8
Manuel García Cremades <mgarcia.severodam@hotmail.com>
2022-05-17 00:13:36 +0200

    añadidos los metodos subir, bajar, derecha e izquierda y ref control

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -2,7 +2,7 @@ package net.instituto.desa;
 
 public class Jugador {
 	int y, x;
-
+	
 	private int getY() {
 		return y;
 	}
@@ -18,5 +18,25 @@ public class Jugador {
 	private void setX(int x) {
 		this.x = x;
 	}
+	
+	public void subir(int power) {
+        int actual = Jugador.this.getY();
+        Jugador.this.setY(actual + power);
+    }
+	
+	public void bajar(int power) {
+        int actual = Jugador.this.getY();
+        Jugador.this.setY(actual - power);
+    }
+	
+	public void derecha(int power) {
+        int actual = Jugador.this.getX();
+        Jugador.this.setX(actual + power);
+    }
+	
+	public void izquierda(int power) {
+        int actual = Jugador.this.getX();
+        Jugador.this.setX(actual - power);
+    }
 
 }
